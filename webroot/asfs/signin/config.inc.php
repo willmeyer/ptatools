06b9977bbb9a7e398549ab090c609d4ec40e846f
Will Meyer <will@willmeyer.com>
2011-12-28 11:03:55 -0500

    fixed config change

--- webroot/asfs/signin/config.inc.php
+++ webroot/asfs/signin/config.inc.php
@@ -2,10 +2,11 @@
 
 //define("DBGLOG_ENABLED", true); 
 define("CREDS_USERNAME", "frontdesk"); 
-define("CREDS_PASSWORD", "asfsrules!"); 
-define("CREDS_DEVICECODE", "asfsrules!"); 
-define("MONGO_CONNECTION", "mongodb://app:asfsrules!@staff.mongohq.com:10060/asfs"); 
+define("CREDS_PASSWORD", "changeme"); 
+define("CREDS_DEVICECODE", "changeme"); 
+define("MONGO_CONNECTION", "mongodb://app:changeme@staff.mongohq.com:10060/asfs"); 
 
 date_default_timezone_set("America/New_York");
+
 
 ?>
